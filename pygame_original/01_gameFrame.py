2b47caff5915dbf77d388e635efc2fd5955a2df6
librabit <codeplay.song@gmail.com>
2022-04-03 08:37:38 +0900

    1. 창을 만들고 닫는 이벤트

--- pygame_original/01_gameFrame.py
+++ pygame_original/01_gameFrame.py
@@ -6,8 +6,18 @@ pygame.init() # 초기화 (반드시 필요)
 
 #화면크기 설정
 screen_width = 480 # 가로크기
-screen_height = 640 # 세로크키
+screen_height = 640 # 세로크기
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-#화면 타이틀 설정 (제목창)
+#화면 타이틀 (제목창)
 pygame.display.set_caption("똥피하기-코드플레이")
+
+#이벤트 루프 - 종료까지 대기
+running = True #실행중인지 확인
+while running:
+    for event in pygame.event.get(): #키마 이벤트를 지속적으로 체크
+        if event.type == pygame.QUIT: #창닫는 이벤트
+            running = False
+
+#종료처리
+pygame.quit()
