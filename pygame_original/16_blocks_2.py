5d9e56e96fcacb0dc040014b0a4d23d1ca463e1b
librabit <codeplay.song@gmail.com>
2022-06-18 14:39:33 +0900

    6/18 퐁게임 바닥죽기남음 (민재현수)

--- pygame_original/16_blocks_2.py
+++ pygame_original/16_blocks_2.py
@@ -18,9 +18,7 @@ bar_height = 25
 
 bar_xPos = screen_width / 2 - bar_width / 2
 bar_yPos = screen_height - bar_height
-
 bar_rect = pygame.Rect(bar_xPos, bar_yPos, bar_width, bar_height)
-
 bar_to_X = 0
 
 #2. 공 정의
@@ -28,7 +26,6 @@ ball_size = 20
 
 ball_xPos = screen_width / 2
 ball_yPos = screen_height - bar_height - ball_size
-
 ball_rect = pygame.Rect(ball_xPos, ball_yPos, ball_size * 2, ball_size * 2)
 ball_rect.center = (ball_xPos, ball_yPos)
 
@@ -42,16 +39,16 @@ block_height = screen_height / 20
 block_xPos = 0
 block_yPos = 0
 
-blocks = [[] for _ in range(10)] #[[] for i in range(10)] #10개의 빈 리스트 생성
+blocks = [[0, b, c], [], [], [], [], [], [], [], [], []] #[[] for i in range(10)] #10개의 빈 리스트 생성
 block_color = [[], [], [], [], [], [], [], [], [], []] #10개의 빈 리스트 생성
+blocks[0] = [a, b, c]
+blocks[0][0] = a
 
 for i in range(10):
     for j in range(3):
         blocks[i].append(pygame.Rect(i*block_width, j*block_height, block_width, block_height))
         block_color[i].append((random.randrange(256), random.randrange(256), random.randrange(256)))
-
 print(blocks)
-
 #4. 바와 마우스 움직임 정의
 mouse_xPos = 0
 mouse_yPos = 0
@@ -63,16 +60,33 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         
+        # if event.type == pygame.KEYDOWN:
+        #     if event.key == pygame.K_LEFT:
+        #         bar_to_X -= 1
+        #     elif event.key == pygame.K_RIGHT:
+        #         bar_to_X += 1
+
+        # if event.type == pygame.KEYUP:
+        #     if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+        #         to_x = 0
+
         if event.type == pygame.MOUSEMOTION: #마우스 위치에 따라 좌우로 막대 이동
             mouse_xPos, mouse_yPos = pygame.mouse.get_pos()
             if mouse_xPos - bar_width / 2 >= 0 and mouse_xPos + bar_width / 2 <= screen_width:
                 bar_xPos = mouse_xPos - bar_width / 2
                 bar_rect.left = mouse_xPos - bar_width / 2
-    
     #배경그리기
     screen.fill((200, 200, 100))
 
     #막대기 그리기
+    bar_xPos += bar_to_X
+
+    if bar_xPos < 0:
+        bar_xPos = 0
+    if bar_xPos > screen_width - bar_width:
+        bar_xPos = screen_width - bar_width
+    bar_rect.left = bar_xPos
+    
     pygame.draw.rect(screen, (90, 90, 255), bar_rect)
 
     #공 튕기기
@@ -95,7 +109,7 @@ while running:
     
     # 충돌판정 : 막대기와 충돌
     if ball_rect.colliderect(bar_rect):
-        ball_y_speed = -ball_y_speed    
+        ball_y_speed *= -1  
     
     #벽돌그리기
     for i in range(10):
@@ -108,7 +122,8 @@ while running:
                     ball_x_speed *= -1
                     ball_y_speed *= -1
                     blocks[i][j] = 0 #블럭의 정보를 삭제
-                    
+    print(blocks)                
     pygame.display.update()
+
 #종료처리
 pygame.quit()
