aeff6d567c13d5f0d7b684efc2c515290ed1bac0
librabit <codeplay.song@gmail.com>
2022-07-09 12:25:11 +0900

    7/9 코딩시험 2박2정

---
+++ python_exam/01_character.py
@@ -0,0 +1,19 @@
+import random
+
+character = []
+running = True
+
+while running:
+    data = input("명령을 입력하세요 : ")
+    if data == "":
+        if len(character) == 10:
+            pass
+        else:
+            pass
+    elif data == "":
+        pass
+    elif data == "":
+        pass
+    elif data == "":
+        print("프로그램을 종료합니다")
+        running = False
