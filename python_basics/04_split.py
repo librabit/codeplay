71421be3aeea0991abe0890a2086839fa0f96ee8
librabit <codeplay.song@gmail.com>
2022-09-17 16:14:49 +0900

    9/17 2김1정 문자열스플릿 완료(고급계산기)

---
+++ python_basics/04_split.py
@@ -0,0 +1,21 @@
+result = 0
+input_data = 0
+while True:
+    input_data = input("궁금한 계산을 물어봐 : ")
+    if "+" in input_data:
+        result = int(input_data.split("+")[0]) + int(input_data.split("+")[1])
+    elif "-" in input_data:
+        result = int(input_data.split("-")[0]) - int(input_data.split("-")[1])
+    elif "*" in input_data:
+        result = int(input_data.split("*")[0]) * int(input_data.split("*")[1])
+    elif "/" in input_data:
+        result = int(input_data.split("/")[0]) / int(input_data.split("/")[1])
+    elif "꺼져" in input_data:
+        print("응 니얼굴~")
+        break
+    else:
+        print("계산식을 넣으라고 이 좌쉬가!!!")
+        continue
+    print(f"{input_data} = {result}")
+
+    ["56 ", " 9"]
