7f756ef999e4bacab99b814558914cd8a90c20c4
librabit <codeplay.song@gmail.com>
2022-12-03 16:21:31 +0900

    12/3 가위바위보 전원 성적 나쁘지않음 (정시우또빠짐)

--- python_basics/rsp.py
+++ python_basics/rsp.py
@@ -26,21 +26,68 @@
 
 
 
+# import random
+# user = 0
+# com = 0
+# com_choice = ["가위", "바위", "보"]
+# running = True
+
+# while running:
+#     user = input("가위바위보 합시다 : ")
+#     com = random.choice(com_choice)
+#     if user == "q":
+#         running = False
+#     else:
+#         print(f"유저 : {user} / 컴퓨터 : {com}")
+#         if user == com:
+#             print("무승부")
+#         else:
+#             if user == "가위":
+#                 if com == "바위":
+#                     print("컴 승")
+#                 else:
+#                     print("유저 승")
+#             if user == "바위":
+#                 if com == "보":
+#                     print("컴 승")
+#                 else:
+#                     print("유저 승")
+#             if user == "보":
+#                 if com == "가위":
+#                     print("컴 승")
+#                 else:
+#                     print("유저 승")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import random
 user = 0
 com = 0
-com_choice = ["가위", "바위", "보"]
+rsp = ["가위", "바위", "보"]
 running = True
 
+
 while running:
-    user = input("가위바위보 합시다 : ")
-    com = random.choice(com_choice)
+    com = rsp[random.randint(0,2)] # com = random.choice(rsp)
+    user = input("가위바위보 하실? (끝내기 q) : ")
     if user == "q":
         running = False
     else:
-        print(f"유저 : {user} / 컴퓨터 : {com}")
-        if user == com:
-            print("무승부")
+        print(f"컴 : {com} / 유저 : {user}")
+        if com == user:
+            print("비김")
         else:
             if user == "가위":
                 if com == "바위":
