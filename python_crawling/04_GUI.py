3021c5897a1cdc55882a082cf004be185c57d00d
librabit <codeplay.song@gmail.com>
2022-12-12 22:27:33 +0900

    4. 입력창에 계산기 만들어 보기

--- python_crawling/04_GUI.py
+++ python_crawling/04_GUI.py
@@ -8,9 +8,12 @@ lol.geometry("800x600") #창의 크기 설정
 dp = tk.Entry(lol, width = 100) #input 창 만들기. 
 dp.pack() #dp의 위치 지정. pack의 매개변수로 나중에 설정
 
-def calc(event):
-    print(tk.Entry.get(dp))
+def calc(event): #입력창에 내용 입력받기
+    result = eval(tk.Entry.get(dp))
+    print(result)
+    dp.delete(0, tk.END) #입력창 내용 지우기
+    dp.insert(0, result) #입력창에 내용 넣기
 
-lol.bind("<return>", calc)
+lol.bind("<Return>", calc) #calc에서 입력한 내용을 엔터를 치면 커맨드라인으로 가져옴
 
 lol.mainloop() # 여기까지 창띄우고 보여질 내용
