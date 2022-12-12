cbc1b06447457f701fd254c322cc1b0774abe82c
librabit <codeplay.song@gmail.com>
2022-12-12 22:48:29 +0900

    5. 텍스트 화면에 보여주기

--- python_crawling/04_GUI.py
+++ python_crawling/04_GUI.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.font as ft
 
 lol = tk.Tk() # Tk 객체 생성
 
@@ -8,11 +9,19 @@ lol.geometry("800x600") #창의 크기 설정
 dp = tk.Entry(lol, width = 100) #input 창 만들기. 
 dp.pack() #dp의 위치 지정. pack의 매개변수로 나중에 설정
 
+fontconfig = ft.Font(family = "Malgun Gothic", size = 15, weight = "bold")
+
+def label_show(label): #텍스트상자 만들기
+    show = tk.Label(lol, text = label, fg="white", bg="black", width = 20)
+    show.configure(font = fontconfig)
+    show.pack(side = "bottom") #텍스트상자의 위치. 상하좌우 상대좌표. 절대좌표로 하고프면 pack이 아니라 place로(x-y좌표시스템)
+
 def calc(event): #입력창에 내용 입력받기
-    result = eval(tk.Entry.get(dp))
+    result = tk.Entry.get(dp)
     print(result)
     dp.delete(0, tk.END) #입력창 내용 지우기
-    dp.insert(0, result) #입력창에 내용 넣기
+    # dp.insert(0, result) #입력창에 내용 넣기
+    label_show(result) #텍스트상자에 내용 전달
 
 lol.bind("<Return>", calc) #calc에서 입력한 내용을 엔터를 치면 커맨드라인으로 가져옴
 
