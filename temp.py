4b6af7f6b9c6b37374ec7368eb6e170077b79edc
librabit <codeplay.song@gmail.com>
2023-09-18 18:20:03 +0900

    9/18 준우팀 함수의 4유형

--- temp.py
+++ temp.py
@@ -1,1 +1,29 @@
-print("홍원기 조용히 해")
+import random
+
+def add(a, b):#매개변수 parameter
+    total = a + b
+    return total
+
+print(add(10, 20))
+c = add(10, 20)
+print(c)
+
+def add2(a, b):
+    print(a + b)
+d = add2(100, 200)
+print(d)
+
+def wawa():
+    for i in range(3):
+        print("재형아 이거 써")
+    return "와와 실행 완료"
+
+wawa()
+f = wawa()
+print(f)
+
+def dogsori():
+    for i in range(random.randint(1, 10)):
+        print("멍!" * random.randint(1, 3))
+
+print(dogsori())
