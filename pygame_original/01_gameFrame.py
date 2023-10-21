a3654f4a48ca7ed1889f7bf97e67fc7f17c2728d
librabit <codeplay.song@gmail.com>
2023-10-21 15:34:45 +0900

    10/21 2시1선 파이게임 첫시간

--- pygame_original/01_gameFrame.py
+++ pygame_original/01_gameFrame.py
@@ -3,15 +3,12 @@ import pygame
 pygame.init() # 초기화 (반드시 필요)
 
 #화면크기 설정
-screen_width = 480 # 가로크기
-screen_height = 640 # 세로크기
+screen_width = 1500 # 가로크기
+screen_height = 500 # 세로크기
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 #화면 타이틀 (제목창)
-pygame.display.set_caption("컨닝왕")
-
-score = 0
-gauge = 0
+pygame.display.set_caption("코딩쌤초미남")
 
 #이벤트 루프 - 종료까지 대기
 running = True #실행중인지 확인
@@ -19,21 +16,6 @@ while running:
     for event in pygame.event.get(): #키마 이벤트를 지속적으로 체크
         if event.type == pygame.QUIT:
             running = False
-        if gauge <= 50000:
-            if event.type == pygame.KEYDOWN: #키보드 눌림 확인
-                if event.key == pygame.K_SPACE: #왼쪽 화살표
-                    score += 1
-                    # print(score)
-        else:
-            gauge = 0
-            score = 0
-            print("문제풀이 성공")
-                    # pass # 문제풀이 성공으로 이어짐
-        if event.type == pygame.KEYUP: # 키보드에서 손을 뗐을 때 중지
-            if event.key == pygame.K_SPACE:
-                score = 0
-                gauge = 0
-    gauge += score
-    print(gauge)
+
 #종료처리
 pygame.quit()
