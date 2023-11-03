f2d12c7db831ea11d8332555e658f65f6548043b
librabit <codeplay.song@gmail.com>
2023-11-03 19:13:30 +0900

    11/3 셀레니움 네이버웹툰 1차시도

--- python_crawling/01_bs4.py
+++ python_crawling/01_bs4.py
@@ -2,20 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 import lxml
 
-header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0'}
-url = ""
+header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0'}
+url = "https://comic.naver.com/index"
 res = requests.get(url, headers = header) # 위의 주소로 접속해 html 문서를 긁어옴
 res.raise_for_status()
 
-pip install requests
-pip install lxml
-pip install bs4
 
 soup = BeautifulSoup(res.text, "lxml") # 긁어온 html 문서를 lxml 을 이용해 파싱(해석)함
-menu = soup.find("div", attrs = {"class" : "tbl_st scroll_gr"})
-menu2 = menu.findAll("div")
-for i in menu2:
-    print(i.get_text())
+
+
+print(soup)
+
 
 # print(soup.find("div", attrs = {"class" : "tbl_st scroll_gr"}).get_text())
 # print(soup.find("div", attrs = {"class" : "win-lose"}).get_text()) 
--- python_crawling/03_request_selenium.py
+++ python_crawling/03_request_selenium.py
@@ -2,22 +2,30 @@ import time # ?초 기다리기를 위한 모듈
 from selenium.webdriver.common.by import By # 크롬드라이버에서 HTML 태그 속 요소를 찾음
 from selenium.webdriver.common.keys import Keys # 키보드 입력으로 넘겨줌
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 import lxml
 import requests
 from bs4 import BeautifulSoup
 
-browser = webdriver.Chrome()
-user = input("유저 검색 : ")
-url = f"https://www.op.gg/summoners/kr/{user}"
-user = 0
+browser = webdriver.Chrome(ChromeDriverManager().install())
+url = "https://comic.naver.com/webtoon?tab=fri"
 
 browser.get(url)
-# time.sleep(1)
+time.sleep(1)
 
 soup = BeautifulSoup(browser.page_source, "lxml")
-fellow = soup.find_all("td", attrs = {"class" : "name"})
-print("최근 같은팀")
-for i in fellow:
-    print(i.get_text())
-win_lose = soup.find("div", attrs = {"class" : "stats"})
-print(win_lose.get_text())
+
+top3 = soup.find("ul", attrs = {"class" : "TripleRecommendList__triple_recommend_list--vm8_k"})
+t3_title = top3.findAll("span", attrs = {"class" : "ContentTitle__title--e3qXt"})
+t3_author = top3.findAll("a", attrs = {"class" : "ContentAuthor__author--CTAAP"})
+
+# print(len(t3_title))
+for i in range(len(t3_title)):
+    print(f"{i+1}순위 웹툰 제목 : {t3_title[i].text} / 작가 : {t3_author[i].text}")
+
+
+# for j in top3[:10]:
+#     print(j.text)
+
+
+
