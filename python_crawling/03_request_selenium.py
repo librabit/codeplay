0af21ade7da3ce09109441154f072c86414ecd52
librabit <codeplay.song@gmail.com>
2023-11-10 19:10:21 +0900

    11/10 html 태그 설명. 셀프로 찾기 시도1 - 홍석서윤 결석

--- python_crawling/03_request_selenium.py
+++ python_crawling/03_request_selenium.py
@@ -8,18 +8,20 @@ import requests
 from bs4 import BeautifulSoup
 
 browser = webdriver.Chrome(ChromeDriverManager().install())
-url = "https://comic.naver.com/webtoon?tab=sat"
+url = "https://comic.naver.com/webtoon?tab=fri"
 
 browser.get(url)
 time.sleep(1)
 
 soup = BeautifulSoup(browser.page_source, "lxml")
-top3 = soup.find("ul", attrs = {"class" : "TripleRecommendList__triple_recommend_list--vm8_k"})
+top3 = soup.find("ul", attrs = {"class" : "ContentList__content_list--q5KXY"})
+# print(top3.text)
 
 title = top3.findAll("span", attrs = {"class" : "ContentTitle__title--e3qXt"})
 author = top3.findAll("a", attrs = {"class" : "ContentAuthor__author--CTAAP"})
 rate = top3.findAll("span", attrs = {"class" : "Rating__star_area--dFzsb"})
+#                                     {"key1":"value1", "key2":"value2"}
 
-print("----------토요웹툰 추천3개----------")
-for i in range(len(title)):
-    print(f"{i+1} - {title[i].text} || {author[i].text} || {rate[i].text[2:]}")
+print("----------금요웹툰 20개----------")
+for i in range(len(title))[:20]:
+    print(f"{i+1} - {title[i].text} || {author[i].text} || {rate[i].text}")
--- test.py
+++ test.py
@@ -1,20 +1,83 @@
-class 참치선물세트():
-    def __init__(self, 일반, 야채, 고추):
-        self.일반 = 일반
-        self.야채 = 야채
-        self.고추 = 고추
+# # class 참치선물세트():
+# #     def __init__(self, 일반, 야채, 고추):
+# #         self.일반 = 일반
+# #         self.야채 = 야채
+# #         self.고추 = 고추
 
-    def 내용물보기(self, name):
-        print(name)
-        print("일반참치 : " + str(self.일반))
-        print("야채참치 : " + str(self.야채))
-        print("고추참치 : " + str(self.야채))  
+# #     def 내용물보기(self, name):
+# #         print(name)
+# #         print("일반참치 : " + str(self.일반))
+# #         print("야채참치 : " + str(self.야채))
+# #         print("고추참치 : " + str(self.야채))  
 
-class 특별선물세트(참치선물세트):
-    def __init__(self, 일반, 스팸, 올리브유):
-        super().__init__(일반, 0, 0) #상위클래스의 생성자(init)에서 3개의 변수 중 1개만 가져옴
-        self.스팸 = 스팸 #특별선물세트에서만 쓰는 변수 만들기
-        self.올리브유 = 올리브유 #특별선물세트에서만 쓰는 변수 만들기
+# # class 특별선물세트(참치선물세트):
+# #     def __init__(self, 일반, 스팸, 올리브유):
+# #         super().__init__(일반, 0, 0) #상위클래스의 생성자(init)에서 3개의 변수 중 1개만 가져옴
+# #         self.스팸 = 스팸 #특별선물세트에서만 쓰는 변수 만들기
+# #         self.올리브유 = 올리브유 #특별선물세트에서만 쓰는 변수 만들기
 
-특별01 = 특별선물세트(6, 3, 2)
-특별01.내용물보기("특별세트 1호")
+# # 특별01 = 특별선물세트(6, 3, 2)
+# # 특별01.내용물보기("특별세트 1호")
+
+
+# # def solution(papers, K):
+# # 	length = len(papers)
+# # 	for i, paper in enumerate(papers):
+# # 		K -= paper
+# # 		if K < 0:
+# # 			length = i
+# # 	return length
+
+# # papers1 = [2, 4, 2, 3, 1]
+# # K1 = 7
+# # ret1 = solution(papers1, K1)
+
+# # print("solution 함수의 반환 값은", ret1, "입니다.")
+
+# # papers2 = [2, 4, 2, 3, 1]
+# # K2 = 14
+# # ret2 = solution(papers2, K2)
+
+# # print("solution 함수의 반환 값은", ret2, "입니다.")
+
+
+# def solution(cards):
+# 	answer = 0
+# 	color = []
+# 	number = []
+# 	for i in cards:
+# 		color.append(j)
+# 		number.append(int(j))
+# 	print(color, number)
+# 	# if color[0] == color[1] and color[0] == color[2]:
+# 	# 	answer = sum(number) * 3
+# 	# elif color[0] != color[1] and color[0] != color[2] and color[1] != color[2]:
+# 	# 	answer = sum(number)
+# 	# else:
+# 	# 	answer = sum(number) * 2
+# 	# return answer
+	
+# cards1 = [["blue", "2"], ["red", "5"], ["black", "3"]]
+# ret1 = solution(cards1)
+
+# print("solution 함수의 반환 값은", ret1, "입니다.")
+
+# cards2 = [["blue", "2"], ["blue", "5"], ["black", "3"]]
+# ret2 = solution(cards2)
+
+# print("solution 함수의 반환 값은", ret2, "입니다.")
+
+
+
+
+
+words = "아 진짜김민준, 오늘정말김민준, 왕지호개웃겼어김민준진짜"
+
+word_list = words.split()
+
+print(word_list)
+
+lists = ["왕지호", "김민준", "고준석", "이준우"]
+
+names = "바보 ㅋㅋㅋ ".join(lists)
+print(names)
