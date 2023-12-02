88d97deaa655c586b7c973867fc3b1ff9985f483
librabit <codeplay.song@gmail.com>
2023-12-02 16:00:45 +0900

    12/2 파이게임 그리기

--- pygame_original/12_drawing.py
+++ pygame_original/12_drawing.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 import pygame
+import random
 
 ########################################################
 # 파이게임 초기설정 (반드시 초기에 세 해야하는 것)
 pygame.init()
 
 #화면크기 설정
-screen_width = 480 # 가로크기
-screen_height = 640 # 세로크기
+screen_width = 500 # 가로크기
+screen_height = 500 # 세로크기
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 #화면 타이틀 (GUI 제목창)
@@ -20,9 +21,10 @@ clock = pygame.time.Clock()
 # 1. 사용자가 추가하는 내용물들 초기화 (배경, 스프라이트, 좌표, 속도, 폰트, 시간 등)
 
 #이벤트 반복 시작 - 스크래치의 무한반복과 같음
+r = 20
 running = True #실행중인지 확인
 while running:
-    dt = clock.tick(30) #게임화면이 초당 리프레시되는 횟수
+    dt = clock.tick(60) #게임화면이 초당 리프레시되는 횟수
 
     #2 이벤트 처리(키보드 마우스 등 화면조작 관련)
     for event in pygame.event.get(): #키마 이벤트를 지속적으로 체크
@@ -33,8 +35,14 @@ while running:
     screen.fill((255, 255, 255))
 
     # 직선 그리기
-    # pygame.draw.line(screen, (25, 225, 125), (0, 0), (screen_width, screen_height), 30) # (대상, 색상, 시작점, 끝점, 굵기)
-    # pygame.draw.line(screen, (0, 255, 0), (0, screen_height), (screen_width, 0), 30)
+    pygame.draw.line(screen, (0, 0, 0), (0, 0), (screen_width, screen_height), 5) # (대상, 색상, 시작점, 끝점, 굵기)
+    pygame.draw.line(screen, (0, 0, 0), (0, screen_height), (screen_width, 0), 5)
+
+    # measure = screen_width / 10
+
+    # for i in range(10):
+    #     pygame.draw.line(screen, (0, 0, 0), (0, i*measure), (screen_width, i*measure), 2) # 가로줄
+    #     pygame.draw.line(screen, (0, 0, 0), (i*measure, 0), (i*measure, screen_height), 2) # 세로줄
     
     # # 선 여러개 긋기 (반복문 활용)
     # for i in range(0, 480, 30): # 세로줄
@@ -44,12 +52,16 @@ while running:
 
     # # 원 그리기
     # pygame.draw.circle(screen, (0, 255, 100), (screen_width / 2, screen_height / 2), 100) # 채워진 원 (대상, 색상, 중심점, 반지름)
-    # pygame.draw.circle(screen, (255, 0, 0), (screen_width / 2, screen_height / 2), 200, 50) # 테두리 원 (대상, 색상, 중심점, 반지름, 선굵기)
     
+    # pygame.draw.circle(screen, (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), (screen_width / 2, screen_height / 2), r, 5) # 테두리 원 (대상, 색상, 중심점, 반지름, 선굵기)
+    
+
     # # 사각형
     # pygame.draw.rect(screen, (55, 55, 255), (screen_width / 2, screen_height / 2, 100, 100)) # 채워진 사각형 (대상, 색상, 시작점 + 가로크기 + 세로크기)
-    # pygame.draw.rect(screen, (155, 155, 55), (screen_width / 2, screen_height / 2, 100, 200), 5) # 테두리 사각형 (대상, 색상, 시작점 + 가로크기 + 세로크기, 선 굵기)
-    
+    # pygame.draw.rect(screen, (155, 155, 55), ((screen_width / 2), (screen_height / 2), r, r), 5) # 테두리 사각형 (대상, 색상, 시작점 + 가로크기 + 세로크기, 선 굵기)
+    # r += 10
+    # if r > 250:
+    #     r = 20
     # 타원
     # pygame.draw.ellipse(screen, (55, 55, 255), (screen_width / 2, screen_height / 2, 100, 200)) # 채워진 타원 (대상, 색상, 시작점 + 가로크기 + 세로크기)
     # pygame.draw.ellipse(screen, (155, 155, 55), (screen_width / 2, screen_height / 2, 200, 100), 5) # 테두리 타원 (대상, 색상, 시작점 + 가로크기 + 세로크기, 선 굵기)
@@ -58,8 +70,8 @@ while running:
     # pygame.draw.ellipse(screen, (155, 155, 55), (screen_width / 2 - 100, screen_height / 2 - 100, 100, 100), 5) # 테두리 타원 (대상, 색상, 시작점 + 가로크기 + 세로크기, 선 굵기)
     
     # # 다각형
-    pygame.draw.polygon(screen, (0, 0, 0), [[100, 0], [0, 200], [200, 200]]) # 삼각형 / 점은 3개 이상. 윗쪽부터 반시계방향으로 좌표 작성
-    # pygame.draw.polygon(screen, (0, 0, 0), [[100, 0], [0, 100], [0, 200], [100, 300], [200, 200], [200, 100]]) # 육각형. 점의 갯수만큼 추가하면 됨.
+    # pygame.draw.polygon(screen, (0, 0, 0), [[100, 0], [0, 200], [200, 200]]) # 삼각형 / 점은 3개 이상. 윗쪽부터 반시계방향으로 좌표 작성
+    pygame.draw.polygon(screen, (0, 0, 0), [[100, 0], [0, 100], [0, 200], [100, 300], [150, 250], [200, 200], [200, 100]]) # 육각형. 점의 갯수만큼 추가하면 됨.
         
     # 3. 게임 캐릭터 위치 정의
 
