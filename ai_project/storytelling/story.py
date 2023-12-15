a4dc6d6e1042785a666e84c6260129d2e46d2f7d
librabit <codeplay.song@gmail.com>
2023-12-15 19:12:35 +0900

    12/15 8장이미지 클릭넘기기 - 명석결석

--- ai_project/storytelling/story.py
+++ ai_project/storytelling/story.py
@@ -14,26 +14,26 @@ pygame.display.set_caption("이미지 띄우기")
 image_folder = "ai_project/storytelling/image/"
 image_files = sorted(os.listdir(image_folder))
 
-# 텍스트 폰트 설정
-font = pygame.font.Font(None, 36)
+# 한글 폰트 설정
+font_path = "ai_project/storytelling/image/jalan.ttf"  # 한글 폰트 파일의 경로를 지정해주세요
+font = pygame.font.Font(font_path, 50)
 
-# 텍스트 내용 리스트
+# 텍스트 내용 리스트 (한글로 입력)
 texts = [
-    "Alien ship came to Earth",
-    "Alien Attack",
-    "Military Strike Back",
-    "Military Destroyed",
-    "A Boy stand in front of Alien army with a mic",
-    "Boy started to sing and Alien was painful with that sound",
-    "Alien Defeated",
-    "Boy became Global president"
+    "외계인의 우주선이 지구를 침공했다",
+    "문어같이 생긴 외계인, 지구를 공격시작!",
+    "용감한 지구의 군대가 외계인을 공격했지만...",
+    "강력한 외계인의 레이저 광선에 전멸!",
+    "이때 한 소년이 홀연히 마이크를 들고 등장한다",
+    "소년이 노래를 시작하자 문어 외계인은 고통스러워하고...",
+    "소년의 음파 공격으로 외계인의 비행선 모두 파괴!",
+    "소년은 지구를 지킨 공로로, 지구대통령이 되었다."
 ]
 
 # 현재 이미지와 텍스트 인덱스
 current_index = 0
-current_image_path = os.path.join(image_folder, image_files[current_index])
 try:
-    image = pygame.image.load(current_image_path)
+    image = pygame.image.load(os.path.join(image_folder, image_files[current_index]))
 except pygame.error as e:
     print(f"이미지를 로드하는 중 오류 발생: {e}")
     sys.exit()
@@ -47,10 +47,12 @@ while True:
             sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN:
             # 마우스 클릭 시 다음 이미지로 전환
-            current_index = (current_index + 1) % len(image_files)
-            current_image_path = os.path.join(image_folder, image_files[current_index])
+            current_index += 1
+            if current_index >= len(image_files):
+                pygame.quit()
+                sys.exit()
             try:
-                image = pygame.image.load(current_image_path)
+                image = pygame.image.load(os.path.join(image_folder, image_files[current_index]))
             except pygame.error as e:
                 print(f"이미지를 로드하는 중 오류 발생: {e}")
                 sys.exit()
