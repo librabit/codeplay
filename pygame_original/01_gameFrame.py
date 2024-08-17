76676ed39e04a12c3e4fb4455f1cebb4e989989a
librabit <codeplay.song@gmail.com>
2024-08-17 14:12:55 +0900

    부분수정

--- pygame_original/01_gameFrame.py
+++ pygame_original/01_gameFrame.py
@@ -8,7 +8,7 @@ screen_height = 500
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 #화면 타이틀 (제목창)
-pygame.display.set_caption("코딩쌤초미남")
+pygame.display.set_caption("준우경진")
 
 #이벤트 루프 - 종료까지 대기
 running = True #실행중인지 확인
