19dab73499d04ee3c074f048b663a41703d6fbb3
librabit <codeplay.song@gmail.com>
2024-08-24 16:02:45 +0900

    양평군 인구분포 2개까지 보기 (지현결석)

--- python_data_A/03_myTown.py
+++ python_data_A/03_myTown.py
@@ -12,22 +12,27 @@ rc('font', family=font)
 f = open('python_data_A/csv_data/age.csv', 'r', encoding='utf8')
 data = csv.reader(f)
 yp = []
-area = input("지역명 입력 : ")
-for row in data :
-    if area in row[0]:
-        print(area)
-        print(row[0])
-        # for i in row[3:]:
-        #     yp.append(int(i))
+yp_select = []
+yp_names = []
 
-area = input("지역명 입력 : ")
 for row in data :
-    if area in row[0]:
-        print(area)
-        print(row[0])
+    yp.append(row)
 
-# plt.title(area)
-# plt.style.use('ggplot')
-# plt.plot(yp)
-# plt.legend()
-# plt.show()
+del yp[0:2]
+for i in yp:
+    for j in range(3, len(i)):
+        i[j] = int(i[j])
+
+for i in range(2):
+    area = input("지역명 입력 : ")
+    yp_names.append(area)
+    for local in yp:
+        if area in local[0]:
+            yp_select.append(local[3:])
+
+plt.title("양평군 인구분포도")
+plt.style.use('ggplot')
+for m in range(2):
+    plt.plot(yp_select[m], label=yp_names[m])
+plt.legend()
+plt.show()
