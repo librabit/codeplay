4d0f7a5eab0275ff9abc2d82e6c0494ca4a6fa06
librabit <codeplay.song@gmail.com>
2024-08-24 12:33:09 +0900

    인구통계

--- python_data_A/03_myTown.py
+++ python_data_A/03_myTown.py
@@ -11,15 +11,23 @@ rc('font', family=font)
 
 f = open('python_data_A/csv_data/age.csv', 'r', encoding='utf8')
 data = csv.reader(f)
-ym = []
-area = input("지역명 입력 : ")
-for row in data :
-    if area in row[0]:
-        print("total :", row[1])
-        for i in row[3:]:
-            ym.append(int(i))
+yp = []
+for k in range(2):
+    temp = []
+    area = input("지역명 입력 : ")
+    temp.append(area)
+    for row in data :
+        if area in row[0]:
+            print(row[0])
+            # print("total :", row[1])
+            for i in row[3:]:
+                temp.append(int(i))
+        
+        yp.append(temp)
 
-plt.title(area)
-plt.style.use('ggplot')
-plt.plot(ym)
+# plt.title(area)
+# plt.style.use('ggplot')
+plt.plot(yp[0][1:], label=yp[0][0])
+plt.plot(yp[1][1:], label=yp[1][0])
+plt.legend()
 plt.show()
