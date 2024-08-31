80c30efd31da24a25ae95bfbbb900cd3876c4637
librabit <codeplay.song@gmail.com>
2024-08-31 14:15:26 +0900

    0831 배경과주인공 (백영진결석)

--- pygame_original/06_collision.py
+++ pygame_original/06_collision.py
@@ -42,7 +42,7 @@ to_y = 0
 
 #이동속도 고정해주기
 character_speed = 1
-enemy_speed = 10
+enemy_speed = 1
 
 #이벤트 루프 - 종료까지 대기
 running = True #실행중인지 확인
@@ -56,15 +56,15 @@ while running:
                 to_x -= character_speed
             elif event.key == pygame.K_RIGHT: #오른쪽 화살표 
                 to_x += character_speed
-            # elif event.key == pygame.K_UP: #위쪽 화살표
-            #     to_y -= character_speed
-            # elif event.key == pygame.K_DOWN: #아랫쪽 화살표
-            #     to_y += character_speed
+            elif event.key == pygame.K_UP: #위쪽 화살표
+                to_y -= character_speed
+            elif event.key == pygame.K_DOWN: #아랫쪽 화살표
+                to_y += character_speed
         if event.type == pygame.KEYUP: # 키보드에서 손을 뗐을 때 중지
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT: #가로움직임
                 to_x = 0
-            # elif event.key == pygame.K_UP or event.key == pygame.K_DOWN: #세로움직임
-            #     to_y = 0
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN: #세로움직임
+                to_y = 0
 
     # screen.fill((200, 200, 200)) # 배경을 이미지가 아닌 색으로 지정하는 방법 RGB
     
@@ -87,7 +87,7 @@ while running:
     enemy_yPos += enemy_speed 
     if enemy_yPos > screen_height:
         enemy_yPos = 0
-        enemy_speed = random.randint(5, 15)
+        enemy_speed = random.randint(1, 5)
         enemy_xPos = random.randint(0, screen_width - enemy_width)
 
     #충돌 처리하기
@@ -96,8 +96,8 @@ while running:
     character_rect.top = character_yPos #캐릭터의 실제좌표로 정보를 업데이트
 
     enemy_rect = enemy.get_rect()
-    enemy_rect.left = enemy_xPos
-    enemy_rect.top = enemy_yPos
+    enemy_rect.left = enemy_xPos-100
+    enemy_rect.top = enemy_yPos-100
 
     # 충돌이벤트 체크
     if character_rect.colliderect(enemy_rect): #colliderect는 두개의 사각형이 맞닿는지 체크하는 함수
---
+++ python_data_A/05_bar2.py
@@ -0,0 +1,31 @@
+import csv
+import matplotlib.pyplot as plt
+
+f = open('python_data_A/csv_data/age_mf.csv', 'r', encoding='utf8')
+data = csv.reader(f)
+m = [] # 남성 연령대
+f = [] # 여성 연령대
+
+result = []
+for row in data :
+    if '용문' in row[0] :
+        # print(row)
+        # for i in row[3:104]: # 남성리스트 시작과 끝 
+        #     m.append(int(i))
+        # for i in row[106:]: # 여성리스트 시작과 끝
+        #     f.append(int(i)) 
+
+        for i in range(0, 101) :
+            m.append(-int(row[(i+3)]))
+            f.append(int(row[-(i+1)]))
+m.reverse()
+# print(m, f)
+plt.style.use('ggplot')
+plt.figure(figsize=(10,5), dpi=150)
+plt.rc('font', family='Malgun Gothic')
+plt.rcParams['axes.unicode_minus'] = False
+plt.title("양평군 양평읍 성별분포")
+plt.barh(range(101), m, label = "남성")
+plt.barh(range(101), f, label = "여성")
+plt.legend()
+plt.show()
