07fe4e5485825884bf86f90bd44e323b5f90f5f4
librabit <codeplay.song@gmail.com>
2025-11-07 19:09:27 +0900

    11/7 정호주찬 그리기 (다음시간 태극기)

--- pygame_original/12_drawing.py
+++ pygame_original/12_drawing.py
@@ -26,51 +26,62 @@ while running:
             running = False
     
     # 배경색 설정
-    screen.fill((255, 255, 255))
+    screen.fill((150, 95, 35))
 
     # 직선 그리기
                 #   어따그려?, 무슨색?(3), 시작어디?(2), 끝어디?(2), 굵기는?
-    pygame.draw.line(1,2,3,4,5)
-    pygame.draw.line(screen, (0, 55, 0), (0, 0), (screen_width, screen_height), 50) # (대상, 색상, 시작점, 끝점, 굵기)
-    pygame.draw.line(screen, (0, 0, 0), (0, screen_height), (screen_width, 0), 50)
+    # pygame.draw.line(1,2,3,4,5)
+    # pygame.draw.line(screen, (0, 55, 0), (0, 0), (screen_width, screen_height), 50) # (대상, 색상, 시작점, 끝점, 굵기)
+    # pygame.draw.line(screen, (0, 0, 0), (0, screen_height), (screen_width, 0), 50)
 
-    measure = screen_width / 10
+    # measure = screen_width / 10
 
-    for i in range(10):
-        pygame.draw.line(screen, (0, 0, 0), (0, i*measure), (screen_width, i*measure), 2) # 가로줄
-        pygame.draw.line(screen, (0, 0, 0), (i*measure, 0), (i*measure, screen_height), 2) # 세로줄
+    # for i in range(10):
+    #     pygame.draw.line(screen, (0, 0, 0), (0, i*measure), (screen_width, i*measure), 2) # 가로줄
+    #     pygame.draw.line(screen, (0, 0, 0), (i*measure, 0), (i*measure, screen_height), 2) # 세로줄
     
     # 선 여러개 긋기 (반복문 활용)
-    for i in range(0, 480, 30): # 세로줄
-        pygame.draw.line(screen, (0, 0, 255), (i, 0), (i, 640))
-    for i in range(0, 640, 30): # 가로줄
-        pygame.draw.line(screen, (0, 0, 255), (0, i), (480, i))
+    ll = 760 // 18
+    offset = 22
 
-    # 원 그리기
-    pygame.draw.circle(screen, (0, 255, 100), (screen_width / 2, screen_height / 2), 100) # 채워진 원 (대상, 색상, 중심점, 반지름)
-    pygame.draw.circle(screen, (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), (screen_width / 2, screen_height / 2), r, 50) # 테두리 원 (대상, 색상, 중심점, 반지름, 선굵기)
+    for i in range(0, 800, ll): # 세로줄
+        pygame.draw.line(screen, (0, 0, 0), (i + offset, 0), (i + offset, screen_width))
+    for i in range(0, 800, ll): # 가로줄
+        pygame.draw.line(screen, (0, 0, 0), (0, i + offset), (screen_width, i + offset))
+    pygame.draw.line(screen, (0, 0, 0), (0, screen_height), (screen_width, screen_height), offset * 2)
+    pygame.draw.line(screen, (0, 0, 0), (0, 0), (0, screen_height), offset * 2)
+    pygame.draw.line(screen, (0, 0, 0), (0, 0), (screen_width, 0), offset * 2)
+    pygame.draw.line(screen, (0, 0, 0), (screen_width, 0), (screen_width, screen_height), offset * 2)
+
+    # # 원 그리기
+    start_point = (ll * 3) + offset
     
-    # 사각형
-    pygame.draw.rect(screen, (0, 0, 0), (screen_width / 2, screen_height / 2, 102, 12)) # 채워진 사각형 (대상, 색상, 시작점 + 가로크기 + 세로크기)
-    pygame.draw.rect(screen, (55, 55, 255), ((screen_width / 2)+1, (screen_height / 2) + 1, 80, 10)) # 채워진 사각형 (대상, 색상, 시작점 + 가로크기 + 세로크기)
-    pygame.draw.rect(screen, (155, 155, 55), ((screen_width / 2), (screen_height / 2), r, r), 5) # 테두리 사각형 (대상, 색상, 시작점 + 가로크기 + 세로크기, 선 굵기)
-    r += 10
-    if r > 250:
-        r = 20
-    # 타원
-    pygame.draw.ellipse(screen, (55, 55, 255), (screen_width / 2, screen_height / 2, 100, 200)) # 채워진 타원 (대상, 색상, 시작점 + 가로크기 + 세로크기)
-    pygame.draw.ellipse(screen, (155, 155, 55), (screen_width / 2, screen_height / 2, 200, 100), 5) # 테두리 타원 (대상, 색상, 시작점 + 가로크기 + 세로크기, 선 굵기)
-    pygame.draw.ellipse(screen, (55, 55, 255), (screen_width / 2 - 100, screen_height / 2 - 100, 100, 100)) # 채워진 타원 (대상, 색상, 시작점 + 가로크기 + 세로크기)
-    pygame.draw.ellipse(screen, (155, 155, 55), (screen_width / 2 - 100, screen_height / 2 - 100, 100, 100), 5) # 테두리 타원 (대상, 색상, 시작점 + 가로크기 + 세로크기, 선 굵기)
+    for k in range(3):
+        for i in range(3):
+            pygame.draw.circle(screen, (0, 0, 0), (start_point + (k * (6 * ll)), start_point + (i * (6 * ll))), ll / 8) # 채워진 원 (대상, 색상, 중심점, 반지름)
+    # pygame.draw.circle(screen, (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), (screen_width / 2, screen_height / 2), r, 50) # 테두리 원 (대상, 색상, 중심점, 반지름, 선굵기)
     
-    # # 다각형
-    pygame.draw.polygon(screen, (0, 0, 0), [[100, 0], [0, 200], [200, 200]]) # 삼각형 / 점은 3개 이상. 윗쪽부터 반시계방향으로 좌표 작성
-    pygame.draw.polygon(screen, (0, 0, 0), [[100, 0], [0, 100], [0, 200], [100, 300], [550, 250], [200, 200], [200, 100]], 5) # 육각형. 점의 갯수만큼 추가하면 됨.
+    # # 사각형
+    # pygame.draw.rect(screen, (0, 0, 0), (screen_width / 2, screen_height / 2, 102, 12)) # 채워진 사각형 (대상, 색상, 시작점 + 가로크기 + 세로크기)
+    # pygame.draw.rect(screen, (55, 55, 255), ((screen_width / 2)+1, (screen_height / 2) + 1, 80, 10)) # 채워진 사각형 (대상, 색상, 시작점 + 가로크기 + 세로크기)
+    # pygame.draw.rect(screen, (155, 155, 55), ((screen_width / 2), (screen_height / 2), r, r), 5) # 테두리 사각형 (대상, 색상, 시작점 + 가로크기 + 세로크기, 선 굵기)
+    # r += 10
+    # if r > 250:
+    #     r = 20
+    # # 타원
+    # pygame.draw.ellipse(screen, (55, 55, 255), (screen_width / 2, screen_height / 2, 100, 200)) # 채워진 타원 (대상, 색상, 시작점 + 가로크기 + 세로크기)
+    # pygame.draw.ellipse(screen, (155, 155, 55), (screen_width / 2, screen_height / 2, 200, 100), 5) # 테두리 타원 (대상, 색상, 시작점 + 가로크기 + 세로크기, 선 굵기)
+    # pygame.draw.ellipse(screen, (55, 55, 255), (screen_width / 2 - 100, screen_height / 2 - 100, 100, 100)) # 채워진 타원 (대상, 색상, 시작점 + 가로크기 + 세로크기)
+    # pygame.draw.ellipse(screen, (155, 155, 55), (screen_width / 2 - 100, screen_height / 2 - 100, 100, 100), 5) # 테두리 타원 (대상, 색상, 시작점 + 가로크기 + 세로크기, 선 굵기)
+    
+    # # # 다각형
+    # pygame.draw.polygon(screen, (0, 0, 0), [[100, 0], [0, 200], [200, 200]]) # 삼각형 / 점은 3개 이상. 윗쪽부터 반시계방향으로 좌표 작성
+    # pygame.draw.polygon(screen, (0, 0, 0), [[100, 0], [0, 100], [0, 200], [100, 300], [550, 250], [200, 200], [200, 100]], 5) # 육각형. 점의 갯수만큼 추가하면 됨.
         
     pygame.display.update() # 게임화면을 새로고침해줌.
 
 # 종료시간 살짝 늦추기
-pygame.time.delay(2000)
+# pygame.time.delay(2000)
 
 #종료처리
 pygame.quit() 
--- python_data_A/01_csv.py
+++ python_data_A/01_csv.py
@@ -1,90 +1,103 @@
-import csv
-import matplotlib.pyplot as plt
-f = open('python_data_A\csv_data\yp.csv', 'r', encoding='utf8')
-data = csv.reader(f, delimiter=',')
-print(f, type(f), data, type(data))
-header = next(data)
-print(header)
-
-min = 0 # 최저기온
-min_day = '' # 최저기온 날짜
-max = 0 # 최고기온
-max_day = '' # 최고기온 날짜
-my_min = 0
-my_max = 0
-my_day = "1979-10-26" # 탕탕절
-my_birthday = []
-my_temp = []
-# 2010-01-12
-
-for row in data:
-    row[0] = row[0].lstrip()
-    if row[-2] == '' or row[-1] == '' or row[-3] == '':
-        pass
-    else:
-        # row[-2], row[-1] = float(row[-2]), float(row[-1])
-        # #최저기온찾기
-        # if row[-2] < min:
-        #     min = row[-2]
-        #     min_day = row[0]
-        # #최고기온찾기
-        # if row[-1] > max:
-        #     max = row[-1]
-        #     max_day = row[0]
-        row[2] = float(row[2])
-        if int(row[0][:4]) >= int(my_day[:4]):
-            if row[0][5:] == my_day[5:]:
-                # 년도, 평균기온.
-                my_birthday.append(int(row[0][:4])) # 연도를 숫자로 바꿔넣음
-                my_temp.append(row[2]) # 평균기온을 넣음
-
-        # if row[0] == my_day:
-        #     my_max, my_min = row[-1], row[-2]
-# print(my_birthday, my_temp)
-plt.plot(my_birthday, my_temp, 'r', linestyle='--', label='avr temp')
-plt.plot(my_birthday, my_temp, 'y^', label='dot')
-plt.show()
-# print(f"최저기온 : {min} 날짜 : {min_day}")
-# print(f"최고기온 : {max} 날짜 : {max_day}")
-# print(f"탕탕절 최고기온 : {my_max} 최저기온 : {my_min}")
-f.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import csv #csv 모듈 호출
-# f = open('python_data_A\yangpyeong.csv', 'r', encoding='utf8') # 파일을 open함수로 열어 f(핸들러)에 저장
-# data = csv.reader(f, delimiter=',') # data 라는 csv reader 객체 생성. 이때 파일을 콤마','단위로 구분해 저장
+# import csv
+# import matplotlib.pyplot as plt
+# f = open('', 'r', encoding='utf8')
+# data = csv.reader(f, delimiter=',')
+# print(f, type(f), data, type(data))
 # header = next(data)
 # print(header)
+
+# min = 0 # 최저기온
+# min_day = '' # 최저기온 날짜
+# max = 0 # 최고기온
+# max_day = '' # 최고기온 날짜
+# my_min = 0
+# my_max = 0
+# my_day = "1979-11-06"
+# my_birthday = []
+# my_temp = []
+
+
+# for row in data:
+#     row[0] = row[0].lstrip()
+#     if row[-2] == '' or row[-1] == '' or row[-3] == '':
+#         pass
+#     else:
+#         # row[-2], row[-1] = float(row[-2]), float(row[-1])
+#         # #최저기온찾기
+#         # if row[-2] < min:
+#         #     min = row[-2]
+#         #     min_day = row[0]
+#         # #최고기온찾기
+#         # if row[-1] > max:
+#         #     max = row[-1]
+#         #     max_day = row[0]
+#         row[2] = float(row[2])
+#         if int(row[0][:4]) >= int(my_day[:4]):
+#             if row[0][5:] == my_day[5:]:
+#                 # 년도, 평균기온.
+#                 my_birthday.append(int(row[0][:4])) # 연도를 숫자로 바꿔넣음
+#                 my_temp.append(row[2]) # 평균기온을 넣음
+
+#         # if row[0] == my_day:
+#         #     my_max, my_min = row[-1], row[-2]
+# # print(my_birthday, my_temp)
+# plt.plot(my_birthday, my_temp, 'r', linestyle='--', label='avr temp')
+# plt.plot(my_birthday, my_temp, 'y^', label='dot')
+# plt.show()
+# # print(f"최저기온 : {min} 날짜 : {min_day}")
+# # print(f"최고기온 : {max} 날짜 : {max_day}")
+# # print(f"탕탕절 최고기온 : {my_max} 최저기온 : {my_min}")
+# f.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# csv => comma-seperated value
+
+import csv #csv 모듈 호출
+f = open('python_data_A/csv_data/yp.csv', 'r', encoding='utf8') # 파일을 open함수로 열어 f(핸들러)에 저장
+data = csv.reader(f, delimiter=',') # data 라는 csv reader 객체 생성. 이때 파일을 콤마','단위로 구분해 저장
+# header = next(data)
+# print(header)
+
+birth_date = "2002-06-26"
+temp = []
+
+for i in data:
+    if birth_date in i[0]:
+        # print(i)
+        temp = i[2:]
+# avr, low, high = temp #리스트 언팩킹
+avr, low, high = map(int, map(float, temp))
+
+print(avr, type(avr), low, type(low), high, type(high))
+
 # date = ""
 # temp = -100
 # for row in data:
@@ -95,4 +108,4 @@ f.close()
 #         temp = row[-1]
 #         date = row[0]
 # print(date, temp)
-# f.close()
+f.close()
