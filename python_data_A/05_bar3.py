96a1b1d51e3cdcebf00c1bb880e9cd8f6bd2da43
librabit <codeplay.song@gmail.com>
2025-12-27 15:09:10 +0900

    12/17 준우 양평군 전체비교

---
+++ python_data_A/05_bar3.py
@@ -0,0 +1,48 @@
+# https://www.mois.go.kr/
+import csv
+import matplotlib.pyplot as plt
+
+f = open('python_data_A/csv_data/ypmf.csv', 'r', encoding='utf8')
+data = csv.reader(f)
+
+allM = [] # 전체 남성 연령대
+allF = [] # 전체 여성 연령대
+running = True
+
+for row in data :
+    for i in row[3:104]:
+        if "," in i:
+            i = i.replace(",", "")
+        allM.append(-int(i))
+    for j in row[106:]:
+        if "," in j:
+            j = j.replace(",", "")
+        allF.append(int(j))
+    break
+
+while running:
+    
+    name = input("지역입력 (마치려면 quit 입력)\n => ")
+    
+    if name == 'quit':
+        running = False
+    else:
+
+        for row in data :
+            m = [] # 남성 연령대
+            f = [] # 여성 연령대
+            if name in row[0]:
+                for i in range(101) :
+                    m.append(-int(row[(i+3)]))
+                    f.insert(0, int(row[-(i+1)]))
+            plt.style.use('ggplot')
+            plt.figure(figsize=(10,5), dpi=150)
+            plt.rc('font', family='Malgun Gothic') # 한글을 쓰기위해
+            plt.rcParams['axes.unicode_minus'] = False # 특수기호를 utf-8로 쓰기위해
+            plt.barh(range(101), allM, color = 'skyblue', label = "남성 전체")
+            plt.barh(range(101), allF, color = 'pink', label = "여성 전체")
+            # plt.title(f"양평군 전체 대비 {name}지역 성별분포")
+            # plt.barh(range(101), m, color = 'blue', label = "남성")
+            # plt.barh(range(101), f, color = 'red', label = "여성")
+            plt.legend()
+            plt.show()
--- temp222.py
+++ temp222.py
@@ -1,32 +1,8 @@
-# 0. 어떻게 해결할 지 머릿속으로 알고리즘을 생각해 본다.
-#  - 월요일부터 금요일까지 반복해 비교해본다
-#     - 요일별로 세 가지 조건을 비교해 본다
-#     - 세 가지 조건 중 어느 하나라도 맞지 않으면 그 날은 축구를 못한다
-#  - 일주일 내내 아예 축구를 못하는 날은 -1을 기록한다.
-#  - 축구를 할 수 있는 날은 그날의 요일을 한 칸씩 띄어쓰기해 알려준다.
-
-# 1. 필요한 데이터를 정리한다.
-
-#     1-1. 주어진 데이터를 처리가능하도록 변환
-#     1-2. 주어지지 않은 데이터 중 추가로 생성해야 하는 데이터를 정리
-2. 앞에서 생각한 알고리즘을 코드로 구현한다.
-3. 원하는 조건으로 결론을 제출한다.
-
-weather = list(map(int, input().split()))
-wind = list(map(int, input().split()))
-book = list(map(int, input().split()))
-
-yoil = ["Mon", "Tue", "Wed", "Thu", "Fri"]
-OK = []
-res = ""
-
-for i in range(5):
-    if weather[i] == 0 and wind[i] < 20 and book[i] == 0:
-        OK.append(yoil[i])
-
-if len(OK):
-    res = " ".join(OK)
-else:
-    res = -1
-    
-print(res)
+angle = 0
+for i in range(10):
+    angle += 5
+    print(angle)
+print("- "* 20)
+for i in range(10):
+    angle -= 5
+    print(angle)
