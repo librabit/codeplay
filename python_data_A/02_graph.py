6deeabdb147a3050787b3f07aed72d8a2bb0c3f0
librabit <codeplay.song@gmail.com>
2026-02-20 18:57:26 +0900

    2/20 양평군 데이터 첫 수집

--- python_data_A/02_graph.py
+++ python_data_A/02_graph.py
@@ -1,6 +1,6 @@
 # pip install matplotlib
 
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 # plt.plot([10, 20, 30, 40])
 # plt.show()
@@ -21,44 +21,32 @@
 # plt.show()
 
 
+# plt.title('two graph')
+# plt.plot([10, 20, 30, 40], label='up')    # 증가를 의미하는 up 범례
+# plt.plot([40, 30, 20, 10], label='down')   # 감소를 의미하는 down 범례
+# plt.plot([20, 20, 20, 20], label='zigzag') 
+# plt.legend() # 범례
+# # plt.show()
 
-
-import matplotlib.pyplot as plt # 범례 legend
-plt.title('two graph')
-plt.plot([10, 20, 30, 40], label='up')    # 증가를 의미하는 up 범례
-plt.plot([40, 30, 20, 10], label='down')   # 감소를 의미하는 down 범례
-plt.legend()
-plt.show()
-
-
-
-# import matplotlib.pyplot as plt
+# # import matplotlib.pyplot as plt
 # plt.title('color')  # 제목 설정
-# # 그래프 그리기
 # plt.plot([10, 20, 30, 40], color = 'skyblue', label='skyblue')
-# plt.plot([40, 30, 20, 10], label='magenta', color = 'indigo')
+# plt.plot([40, 30, 20, 10], label='magenta', color = 'magenta')
 # plt.legend()   # 범례 표시
-# plt.show()
+# # plt.show()
 
-
-
-# import matplotlib.pyplot as plt
+# # import matplotlib.pyplot as plt
 # plt.title('linestyle')  # 제목 설정
-# # 빨간색 dashed 그래프
-# plt.plot([10, 20, 30, 40], color ='r', linestyle='soli', label='dashed')
-# # 초록색 dotted 그래프
+# plt.plot([10, 20, 30, 40], color ='r', linestyle='solid', label='solid')
 # plt.plot([40, 30, 20, 10], color ='g', ls=':', label='dotted')
 # plt.legend()  # 범례 표시
-# plt.show()
+# # plt.show() 
 
-
-
-
-import matplotlib.pyplot as plt
+# # import matplotlib.pyplot as plt
 plt.title('marker')  # 제목 설정
-plt.plot([10, 20, 30, 40], 'r.', ls = "--", label = 'circle') # 빨간색 원형 마커 그래프
-plt.plot([40, 30, 20, 10], color = 'black', ls = 'solid', label = 'triangle up') # 초록색 삼각형 마커 그래프
-plt.plot([40, 30, 20, 10], 'g^', label = 'triangle up') 
+plt.plot([10, 20, 30, 40], 'r.', ls = "--", label = 'babo') # 빨간색 원형 마커 그래프
+plt.plot([10, 10, 10, 10], color = 'black', ls = 'solid', label = 'ddonggo') # 초록색 삼각형 마커 그래프
+plt.plot([40, 30, 20, 10], 'g^', label = 'malmizal') 
 plt.legend()  # 범례 표시
 plt.show()
 
--- python_data_A/03_myTown.py
+++ python_data_A/03_myTown.py
@@ -2,40 +2,42 @@
 
 import csv
 import matplotlib.pyplot as plt
-f = open('python_data_A/csv_data/ages.csv', 'r', encoding='utf8')
+
+f = open('python_data_A/csv_data/yp2025.csv', 'r', encoding='utf8')
 data = csv.reader(f, delimiter=',')
 yp = [] #데이터 불러오기
-
-for row in data : # 데이터를 파이썬의 전용 데이터인 리스트로 바꾸기
+print(yp)
+for row in data: # 데이터를 파이썬의 전용 데이터인 리스트로 바꾸기
     yp.append(row)
 
-for i in yp: # 데이터 내의 자료형을 문자열에서 숫자로 몽땅 바꿔주기
-    for j in range(3, len(i)):
-        if ',' in i[j]:
-            i[j] = i[j].replace(",", "")
-        i[j] = int(i[j])
+print(yp)
+# for i in yp: # 데이터 내의 자료형을 문자열에서 숫자로 몽땅 바꿔주기
+#     for j in range(3, len(i)):
+#         if ',' in i[j]:
+#             i[j] = i[j].replace(",", "")
+#         i[j] = int(i[j])
 
-while True:
-    yp_select = []
-    yp_names = []
-    for bigyo in range(2):
-        area = input("지역명 입력 : ")
-        if area == "끝":
-            break
-        yp_names.append(area)
-        for local in yp:
-            if area in local[0]:
-                yp_select.append(local[3:])
-                break
+# while True:
+#     yp_select = []
+#     yp_names = []
+#     for bigyo in range(2):
+#         area = input("지역명 입력 : ")
+#         if area == "끝":
+#             break
+#         yp_names.append(area)
+#         for local in yp:
+#             if area in local[0]:
+#                 yp_select.append(local[3:])
+#                 break
         
-    plt.rc('font', family='Malgun Gothic')
-    plt.title("양평군 인구분포도")
-    plt.style.use('ggplot')
+#     plt.rc('font', family='Malgun Gothic')
+#     plt.title("양평군 인구분포도")
+#     plt.style.use('ggplot')
 
-    area1 = f'{yp_names[0]} (총 인구수 : {sum(yp_select[0])})'
-    area2 = f'{yp_names[1]} (총 인구수 : {sum(yp_select[1])})'
+#     area1 = f'{yp_names[0]} (총 인구수 : {sum(yp_select[0])})'
+#     area2 = f'{yp_names[1]} (총 인구수 : {sum(yp_select[1])})'
     
-    plt.plot(yp_select[0], label = area1)
-    plt.plot(yp_select[1], label = area2)
-    plt.legend()
-    plt.show()
+#     plt.plot(yp_select[0], label = area1)
+#     plt.plot(yp_select[1], label = area2)
+#     plt.legend()
+#     plt.show()
